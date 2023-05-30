e82033709cc7cad107f3837992bb8edda5a2ccab
Beloved-vince <vinceoludare@gmail.com>
2023-05-30 22:14:50 +0100

    task done

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,74 +1,64 @@
-#!/usr/bin/env python3
+"""Route module for the API.
 """
-    Error handling Route module for the API
-    What the HTTP status code for a request unauthorized? 401 of course!
-
-    Edit api/v1/app.py:
-
-    Add a new error handler for this status code, the response must be:
-    a JSON: {"error": "Unauthorized"}
-    status code 401
-    you must use jsonify from Flask
-    For testing this new error handler, add a new endpoint in api/v1/views/index.py:
-
-    Route: GET /api/v1/unauthorized
-    This endpoint must raise a 401 error by using abort - Custom Error Pages
-"""
+import os
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
+
+from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-if os.getenv('AUTH_TYPE') == 'auth':
-    from api.v1.auth.auth import Auth
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
     auth = Auth()
-elif os.getenv('AUTH_TYPE') == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
+if auth_type == 'basic_auth':
     auth = BasicAuth()
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """Not found handler.
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler
+    """Unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden handler
+    """Forbidden handler.
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def before_request_func() -> str:
-    """ Before request handler
+def authenticate_user():
+    """Authenticates a user before processing a request.
     """
-    if auth is None:
-        return
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/',]
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+    if auth:
+        excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            auth_header = auth.authorization_header(request)
+            user = auth.current_user(request)
+            if auth_header is None:
+                abort(401)
+            if user is None:
+                abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,92 +1,38 @@
 #!/usr/bin/env python3
+"""Authentication module for the API.
 """
-Create a folder api/v1/auth
-Create an empty file api/v1/auth/__init__.py
-Create the class Auth:
-in the file api/v1/auth/auth.py
-import request from flask
-class name Auth
-public method def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-    that returns False - path and excluded_paths will be used later, now, you don’t need to take care of them
-public method def authorization_header(self, request=None) -> str:
-    that returns None - request will be the Flask request object
-public method def current_user(self, request=None) -> TypeVar('User'): that returns None - request will be the Flask request object
-"""
-from typing import List, TypeVar, Tuple
-from .auth import Auth
-import base64
-import binascii
+import re
+from typing import List, TypeVar
+from flask import request
 
 
-class BasicAuth(Auth):
+class Auth:
+    """Authentication class.
     """
-    Authentication class
-    BasicAuth that inherits from Auth
-    """
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if a path requires authentication.
         """
-        Extract base64 authorization header method
-        """
-        if authorization_header is None or \
-           type(authorization_header) is not str or \
-           not authorization_header.startswith('Basic '):
-            return None
-        return authorization_header[6:]
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
-    def decode_base64_authorization_header(self,
-                                           base64_authorization_header: str
-                                           ) -> str:
-        """ Decode base64 authorization header method
+    def authorization_header(self, request=None) -> str:
+        """Gets the authorization header field from the request.
         """
-        if base64_authorization_header is None or \
-              type(base64_authorization_header) is not str:
-                return None
-        try:
-            return base64.b64decode(
-                base64_authorization_header).decode('utf-8')
-        except binascii.Error:
-            return None
-        
-    def extract_user_credentials(self,
-                                    decoded_base64_authorization_header: str
-                                    ) -> Tuple['str', 'str']:
-            """
-            user credentials method
-            """
-            if decoded_base64_authorization_header is None or \
-            type(decoded_base64_authorization_header) is not str or \
-            ':' not in decoded_base64_authorization_header:
-                return (None, None)
-            return tuple(decoded_base64_authorization_header.split(':', 1))
-
-    def user_object_from_credentials(self, user_email: str, user_pwd: str
-                                     ) -> TypeVar('User'):
-        """ User object from credentials method
-        """
-        if user_email is None or type(user_email) is not str or \
-           user_pwd is None or type(user_pwd) is not str:
-            return None
-        from models.user import User
-        try:
-            users = User.search({'email': user_email})
-        except Exception:
-            return None
-        if users is None or users == []:
-            return None
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Current user method
+        """Gets the current user from the request.
         """
-        auth_header = self.authorization_header(request)
-        b64_auth_header = self.extract_base64_authorization_header(
-            auth_header)
-        decoded_auth_header = self.decode_base64_authorization_header(
-            b64_auth_header)
-        user_credentials = self.extract_user_credentials(decoded_auth_header)
-        return self.user_object_from_credentials(
-            user_credentials[0], user_credentials[1])
+        return None
